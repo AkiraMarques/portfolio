aee03fe1ffc9077d610b2248e8d8fcf33567a07d
AkiraMarques <marqueslucas504@gmail.com>
2023-10-22 21:10:35 -0300

    adicionado efeito do acordeon e estilizado lista de idiomas

---
+++ assets/JS/acordeon.js
@@ -0,0 +1,15 @@
+// função de abrir e fechar aba iterativa
+const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
+
+acordeonTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
+
+        if(isOpen) {
+            acordeon.classList.remove('open')
+        } else {
+            acordeon.classList.add('open')
+        }
+    })
+})
