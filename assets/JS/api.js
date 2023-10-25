cffe00c5107fd4aa196078de4605db92d3854262
AkiraMarques <marqueslucas504@gmail.com>
2023-10-25 12:39:24 -0300

    ajustando informações no profile.json

--- assets/JS/api.js
+++ assets/JS/api.js
@@ -1,7 +1,8 @@
 
 
-function fetchProfileData() {
-    const url ='';
-    fetch()
+async function fetchProfileData() {
+    const url ='https://raw.githubusercontent.com/AkiraMarques/portfolio/main/data/profile.json';
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
 
